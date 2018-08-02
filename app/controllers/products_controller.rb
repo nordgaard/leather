ecc8c373970e5413468b2e601d72c7f13a29f519
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2018-08-02 16:00:47 -0700

    index working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+
+  def index
+     @products = Product.all
+  end
+
+
+def show
+  @product = Products.find_by(id: params[:id])
+end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+
+	
 end
