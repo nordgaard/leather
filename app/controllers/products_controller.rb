b70a955b2cdc2af16d0383b481bbde171b1d66f3
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2018-08-03 00:19:24 -0700

    Product woring M, Show V, C complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,78 @@
 class ProductsController < ApplicationController
 
-  def product_params
-    params.require(:product).permit(:name)
-  end
+  before_action :authenticate_admin!, except: [:index, :show, :search]
 
   def index
-     @products = Product.all
+    if session[:count] == nil
+      session[:count] = 0
+    end
+    session[:count] += 1
+    @visits = session[:count]
+
+    @products = Product.all
+    sort_attribute = params[:sort]
+    sort_order = params[:order]
+    discount = params[:discount]
+    if sort_attribute && sort_order
+      @products = Product.all.order(sort_attribute => sort_order)
+    end
+    if discount
+      @products = Product.where("price < ?", "3")
+    end
+    if params[:category]
+      @products = Category.find_by(title: params[:category]).products
+    end
+    render "index.html.erb"
   end
 
+#   def new
+#   end
 
-def show
-  @product = Products.find_by(id: params[:id])
+#   def create
+#     @product = Product.new(
+#       name: params[:name],
+#       description: params[:description],
+#       price: params[:price],
+#       supplier_id: params[:supplier]["supplier_id"]
+#     )
+#     if @product.save 
+#       flash[:success] = "Product Created"
+#       redirect_to "/products/#{@product.id}"
+#     else
+#       render :new
+#     end
+#   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+#     if params[:id] == "random"
+#       @product = Product.all.sample
+#     end
+#     render "show.html.erb"
+  end
+
+#   def edit
+#     @product = Product.find_by(id: params[:id])
+#     render "edit.html.erb"
+#   end
+
+#   def update
+#     @product = Product.find_by(id: params[:id])
+#     @product.update(
+#       name: params[:name],
+#       description: params[:description],
+#       image: params[:image],
+#       price: params[:price]
+#     )
+#     flash[:success] = "Product Updated"
+#     redirect_to "/products/#{@product.id}"
+#   end
+
+#   def destroy
+#     @product = Product.find_by(id: params[:id])
+#     @product.destroy
+#     flash[:warning] = "Product Destroyed"
+#     redirect_to "/"
+#   end
 end
 
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731212602) do
+ActiveRecord::Schema.define(version: 20180803065712) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.float    "price"
     t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
