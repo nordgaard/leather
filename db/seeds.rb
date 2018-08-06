903fe6131a94109a3bdbc8570f059f1b7afd8cda
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2018-08-05 23:43:36 -0700

    cant get the seed to go through, added faker and seed:dump gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-Product.create!([
-  {name: "Tassle", price: 10.0, quantity: nil, description: nil}
-])
+Product.destroy_all
+
+100.times do |index|
+  Product.create!(description: Faker::Lorem.sentence(3, false, 0).chop,
+                name: Faker::GratefulDead.song,
+                price: Faker::Number.between(1, 100)))
+	end
+end
